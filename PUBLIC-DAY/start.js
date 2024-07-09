66c50f54da4308d5c92927d786e7284cb8a8f8db
mariesteinmetz <159348367+mariesteinmetz@users.noreply.github.com>
2024-07-09 21:49:41 +0200

    Add files via upload

---
+++ PUBLIC-DAY/start.js
@@ -0,0 +1,86 @@
+function openPopup(popupId) {
+  document.getElementById(popupId).style.display = 'block';
+
+  document.querySelector('.bartender-button').classList.add('no-hover');
+}
+
+function closePopup(popupId) {
+  document.getElementById(popupId).style.display = 'none';
+};
+
+/* disability sign */
+const disabilitySignButton = document.getElementById('disability-sign-button');
+const closeDisabilitySignPopup = document.getElementById('close-disability-sign-popup');
+const disabilitySignPopup = document.getElementById('disability-sign-popup'); 
+const disabilitySignOverlay = document.getElementById('disability-sign-overlay');
+
+disabilitySignButton.addEventListener('click', (event) => {
+  disabilitySignPopup.classList.add('active'); 
+  disabilitySignOverlay.classList.add('active');
+}); 
+
+closeDisabilitySignPopup.addEventListener('click', (event) => {
+  disabilitySignPopup.classList.remove('active');
+  disabilitySignOverlay.classList.remove('active');
+});
+
+/* imprint */
+const imprintButton = document.getElementById('imprint-button');
+const closeImprintPopup = document.getElementById('close-imprint-popup');
+const imprintPopup = document.getElementById('imprint-popup'); 
+const imprintOverlay = document.getElementById('imprint-overlay'); 
+
+imprintButton.addEventListener('click', (event) => {
+  imprintPopup.classList.add('active'); 
+  imprintOverlay.classList.add('active');
+});
+
+closeImprintPopup.addEventListener('click', (event) => {
+  imprintPopup.classList.remove('active');
+  imprintOverlay.classList.remove('active');
+});
+
+
+/* about */
+const homeButton = document.getElementById('home-button');
+const closeHomePopup = document.getElementById('close-home-popup');
+const homePopup = document.getElementById('home-popup'); 
+const homeOverlay = document.getElementById('home-overlay'); 
+
+homeButton.addEventListener('click', (event) => {
+  homePopup.classList.add('active'); 
+  homeOverlay.classList.add('active');
+});
+
+closeHomePopup.addEventListener('click', (event) => {
+  homePopup.classList.remove('active');
+  homeOverlay.classList.remove('active');
+});
+
+/* private */
+const privateButton = document.getElementById('private-button'); 
+
+privateButton.addEventListener('click', (event) => { 
+  window.location.href = '/Website/PRIVATE/00_START/start.html';
+});
+
+/* semi-public */
+const semiPublicButton = document.getElementById('semi-public-button'); 
+
+semiPublicButton.addEventListener('click', (event) => { 
+  window.location.href = '/Website/SEMI-PUBLIC/00_START/start.html';
+});
+
+/* semi-private */
+const semiPrivateButton = document.getElementById('clock-button'); 
+
+semiPrivateButton.addEventListener('click', (event) => { 
+  window.location.href = '/Website/SEMI-PRIVATE/00_START/start.html';
+});
+
+/* night */
+const nightButton = document.getElementById('night-button'); 
+
+nightButton.addEventListener('click', (event) => { 
+  window.location.href = '/Website/PUBLIC-NIGHT/00_START/start.html';
+});
