6d9c8afb49fa057d337ce96a8ed77c2e4ecc2cc7
mariesteinmetz <159348367+mariesteinmetz@users.noreply.github.com>
2024-09-27 19:24:56 +0200

    Create start.js

---
+++ PUBLIC-DAY/start.js
@@ -0,0 +1,86 @@
+function openPopup(popupId) {
+  document.getElementById(popupId).style.display = 'block';
+
+  document.querySelector('.bartender-button').classList.add('no-hover');
+}
+
+function closePopup(popupId) {
+  document.getElementById(popupId).style.display = 'none';
+};
+
+/* vespasienne */
+const vespasienneButton = document.getElementById('vespasienne-button');
+const closeVespasiennePopup = document.getElementById('close-vespasienne-popup');
+const vespasiennePopup = document.getElementById('vespasienne-popup'); 
+const vespasienneOverlay = document.getElementById('vespasienne-overlay');
+
+vespasienneButton.addEventListener('click', (event) => {
+  vespasiennePopup.classList.add('active'); 
+  vespasienneOverlay.classList.add('active');
+}); 
+
+closeVespasiennePopup.addEventListener('click', (event) => {
+  vespasiennePopup.classList.remove('active');
+  vespasienneOverlay.classList.remove('active');
+});
+
+/* imprint */
+const imprintButton = document.getElementById('imprint-button');
+const closeImprintPopup = document.getElementById('close-imprint-popup');
+const imprintPopup = document.getElementById('imprint-popup'); 
+const imprintOverlay = document.getElementById('imprint-overlay'); 
+
+imprintButton.addEventListener('click', (event) => {
+  imprintPopup.classList.add('active'); 
+  imprintOverlay.classList.add('active');
+});
+
+closeImprintPopup.addEventListener('click', (event) => {
+  imprintPopup.classList.remove('active');
+  imprintOverlay.classList.remove('active');
+});
+
+
+/* about */
+const homeButton = document.getElementById('home-button');
+const closeHomePopup = document.getElementById('close-home-popup');
+const homePopup = document.getElementById('home-popup'); 
+const homeOverlay = document.getElementById('home-overlay'); 
+
+homeButton.addEventListener('click', (event) => {
+  homePopup.classList.add('active'); 
+  homeOverlay.classList.add('active');
+});
+
+closeHomePopup.addEventListener('click', (event) => {
+  homePopup.classList.remove('active');
+  homeOverlay.classList.remove('active');
+});
+
+/* private */
+const privateButton = document.getElementById('private-button'); 
+
+privateButton.addEventListener('click', (event) => { 
+  window.location.href = '/Website/PRIVATE/00_START/start.html';
+});
+
+/* semi-public */
+const semiPublicButton = document.getElementById('semi-public-button'); 
+
+semiPublicButton.addEventListener('click', (event) => { 
+  window.location.href = '/Website/SEMI-PUBLIC/start.html';
+});
+
+/* semi-private */
+const semiPrivateButton = document.getElementById('clock-button'); 
+
+semiPrivateButton.addEventListener('click', (event) => { 
+  window.location.href = '/Website/SEMI-PRIVATE/00_START/start.html';
+});
+
+/* night */
+const nightButton = document.getElementById('night-button'); 
+
+nightButton.addEventListener('click', (event) => { 
+  window.location.href = '/Website/PUBLIC-NIGHT/start.html';
+});
